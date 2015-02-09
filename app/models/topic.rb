28a20911f6503fc6dd095c3b6379a51a64004aae
kris utter <krisu@teamddm.com>
2015-02-09 10:26:41 -0500

    added type to import

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -24,47 +24,47 @@ class Topic < ActiveRecord::Base
       end
 
       if xls
-        xls.each(:ad => '^Ad$',:description_1 => '^Description line 1$',:description_2 => '^Description line 2$',:ad_group => '^Ad group$', :campaign => '^Campaign$', :display_url => '^Display URL$', :destination_url => '^Destination URL', :ad_id => "^Ad ID$", :ad_group_id => "^Ad group ID$") do |hash|
-          
-          unless hash[:campaign] == " --" || hash[:campaign] == "Campaign" || hash[:campaign] == nil
-            topic = Topic.find_by_name hash[:campaign]
-            if !topic
-                topic = Topic.new
-                topic.name = hash[:campaign]
-                puts "New #{topic.name}"
+        xls.each(:ad => '^Ad$',:description_1 => '^Description line 1$',:description_2 => '^Description line 2$',:ad_group => '^Ad group$', :campaign => '^Campaign$', :display_url => '^Display URL$', :destination_url => '^Destination URL', :ad_id => "^Ad ID$", :ad_group_id => "^Ad group ID$", :ad_type => "^Ad type$") do |hash|
+          if hash[:ad_type] == "Ad type"
+            unless hash[:campaign] == " --" || hash[:campaign] == "Campaign" || hash[:campaign] == nil
+              topic = Topic.find_by_name hash[:campaign]
+              if !topic
+                  topic = Topic.new
+                  topic.name = hash[:campaign]
+                  puts "New #{topic.name}"
+                  
+              end
+
+              topic.save
                 
+              group = Group.find_by_ad_group_id hash[:ad_group_id]
+              if !group
+                group = Group.new
+              end
+              group.name = hash[:ad_group]
+              group.ad_group_id = hash[:ad_group_id]
+              group.destination_url = hash[:destination_url]
+              group.display_url = hash[:display_url]
+              group.network = "Google"
+              topic.groups << group
+
+              group.save
+
+              content = hash[:ad] + "\n" + hash[:description_1] + "\n" + hash[:description_2]
+
+              copy = Copy.find_by_ad_id hash[:ad_id]
+              if !copy
+                copy = Copy.new 
+                copy.selected = true
+              end
+
+              copy.ad_id = hash[:ad_id]
+              copy.content = content
+              copy.editor = @user.email 
+              group.copies << copy
+
             end
-
-            topic.save
-              
-            group = Group.find_by_ad_group_id hash[:ad_group_id]
-            if !group
-              group = Group.new
-            end
-            group.name = hash[:ad_group]
-            group.ad_group_id = hash[:ad_group_id]
-            group.destination_url = hash[:destination_url]
-            group.display_url = hash[:display_url]
-            group.network = "Google"
-            topic.groups << group
-
-            group.save
-
-            content = hash[:ad] + "\n" + hash[:description_1] + "\n" + hash[:description_2]
-
-            copy = Copy.find_by_ad_id hash[:ad_id]
-            if !copy
-              copy = Copy.new 
-              copy.selected = true
-            end
-
-            copy.ad_id = hash[:ad_id]
-            copy.content = content
-            copy.editor = @user.email 
-            group.copies << copy
-
           end
-
         end
       end
     else
