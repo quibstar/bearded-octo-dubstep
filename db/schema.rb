ef4d04d0478a5eba7d42db68bc6325181fd38530
kris utter <krisu@teamddm.com>
2015-08-27 13:11:49 -0400

    added checkbox for not editing front end content

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424132423) do
+ActiveRecord::Schema.define(version: 20150827163520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 20150424132423) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "can_edit",   default: true
   end
 
   create_table "collections", force: true do |t|
